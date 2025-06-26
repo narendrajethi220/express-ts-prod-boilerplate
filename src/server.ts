53be81247faa2dff6632655a115052238a706682
Narendra Jethi <narendrajethi220@gmail.com>
2025-06-26 06:49:16 +0530

    added initial winston logger setup (under development)

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { serverConfig } from "./config";
 import pingRouter from "./routers/v1/index.router";
 import { genericErrorHandler } from "./middlewares/error.middleware";
+import logger from "./config/logger.config";
 
 const app = express();
 const PORT = serverConfig.PORT;
@@ -14,5 +15,6 @@ app.use("/api/v1", pingRouter);
 app.use(genericErrorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server is 🚀 on ${PORT}`);
+  logger.info(`Server is 🚀 on ${PORT}`);
+  logger.info("Press Ctrl + C to stop the server");
 });
